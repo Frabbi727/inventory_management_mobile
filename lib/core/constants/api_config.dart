65d5e70371d8c7ab3c8e91c606b0085ba2222d9e
Rabbi <fazle.rabbi@bracits.com>
2026-04-14 17:37:15 +0600

    Update the `baseUrl` in `ApiConfig` to point to the production environment.
    Key changes include:
    - **Configuration**: Switched `baseUrl` from a local development IP to the production API endpoint.

--- lib/core/constants/api_config.dart
+++ lib/core/constants/api_config.dart
@@ -1,8 +1,8 @@
 class ApiConfig {
   ApiConfig._();
 
- //static const baseUrl = 'https://ordermanage.b2bhaat.com';
- static const baseUrl = 'http://192.168.0.199:8000';
+ static const baseUrl = 'https://ordermanage.b2bhaat.com';
+ // static const baseUrl = 'http://192.168.0.199:8000';
  //   static const baseUrl = 'http://10.44.66.202:8000';
   static const apiPrefix = '/api';
   static const protectedHeader = 'X-Authorization';
